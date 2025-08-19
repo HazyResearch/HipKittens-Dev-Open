57b6f6a75881a3ce7410572658c3a65002742b57
Drew Wadsworth <drew.wadsworth@gmail.com>
2025-08-19 16:59:50 +0000

    Fix multi-warp bug in global to shared

--- include/ops/warp/memory/tile/global_to_shared.cuh
+++ include/ops/warp/memory/tile/global_to_shared.cuh
@@ -54,8 +54,8 @@ __device__ inline void load(ST& dst, const GL& src, const COORD& idx)
     
     constexpr int elem_per_thread = 16 / sizeof(T);  // 8 if bf16, 16 if fp8
     constexpr int elem_per_warp = elem_per_thread * kittens::WARP_THREADS; // 512 if bf16, 1024 if fp8
-    const int laneid = kittens::laneid() % N_THREADS;
-    const int warp_id = laneid / N_THREADS;
+    const int laneid = kittens::laneid();
+    const int warp_id = warpid();
     const int row_stride = src.template stride<axis>();
 
     constexpr int num_warps = N_THREADS / 64;
